790e7b00174d6629f0780f06e3d628e5e0db3a6f
Felipe Valencia <fvalencia@vc-soft.com>
2022-07-29 13:40:41 -0500

    Completed FacePainting App

---
+++ FacePainting/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  FacePainting
+//
+//  Created by Felipe Valencia on 30/06/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
